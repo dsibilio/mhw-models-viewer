2b4b259659ec3dd06e7ebe563ff7844f20bf1902
Mizaroth <domenicosibilio@gmail.com>
2020-10-21 12:51:05 +0200

    Add global viewer and remove meshes on model reload

--- src/assets/js/online3dembedder.js
+++ src/assets/js/online3dembedder.js
@@ -1,77 +1,76 @@
-LoadOnline3DModels = function ()
-{
-	function Error (viewerElement, message)
-	{
-		var context = viewerElement.getContext ('2d');
+var viewer;
+
+LoadOnline3DModels = function () {
+	function Error(viewerElement, message) {
+		var context = viewerElement.getContext('2d');
 		context.font = '12px Arial';
-		context.fillText (message, 0, 12);
+		context.fillText(message, 0, 12);
 	}
-	
-	function LoadViewer (viewerElement)
-	{
-		var urls = viewerElement.getAttribute ('sourcefiles');
+
+	function LoadViewer(viewerElement) {
+		var urls = viewerElement.getAttribute('sourcefiles');
 		if (urls === undefined || urls === null) {
-			Error (viewerElement, 'Invalid source files.');
+			Error(viewerElement, 'Invalid source files.');
 			return;
 		}
 
-		var urlList = urls.split ('|');
-		JSM.ConvertURLListToJsonData (urlList, {
-			onError : function () {
-				Error (viewerElement, 'Conversion failed.');
+		var urlList = urls.split('|');
+		JSM.ConvertURLListToJsonData(urlList, {
+			onError: function () {
+				Error(viewerElement, 'Conversion failed.');
 				return;
 			},
-			onReady : function (fileNames, jsonData) {
+			onReady: function (fileNames, jsonData) {
 				var viewerSettings = {
-					cameraEyePosition : [6.0, -5.5, 4.0],
-					cameraCenterPosition : [0.0, 0.0, 0.0],
-					cameraUpVector : [0.0, 0.0, 1.0]
+					cameraEyePosition: [6.0, -5.5, 4.0],
+					cameraCenterPosition: [0.0, 0.0, 0.0],
+					cameraUpVector: [0.0, 0.0, 1.0]
 				};
 
-				var viewer = new JSM.ThreeViewer ();
-				if (!viewer.Start (viewerElement, viewerSettings)) {
-					Error (viewerElement, 'Internal error.');
+				if(!viewer) {
+					viewer = new JSM.ThreeViewer();
+				}
+
+				if (!viewer.Start(viewerElement, viewerSettings)) {
+					Error(viewerElement, 'Internal error.');
 					return;
 				}
 
+				viewer.RemoveMeshes();
 				var currentMeshIndex = 0;
 				var environment = {
-					onStart : function (/*taskCount, meshes*/) {
-						viewer.EnableDraw (false);
+					onStart: function (/*taskCount, meshes*/) {
+						viewer.EnableDraw(false);
 					},
-					onProgress : function (currentTask, meshes) {
+					onProgress: function (currentTask, meshes) {
 						while (currentMeshIndex < meshes.length) {
-							viewer.AddMesh (meshes[currentMeshIndex]);
+							viewer.AddMesh(meshes[currentMeshIndex]);
 							currentMeshIndex = currentMeshIndex + 1;
 						}
 					},
-					onFinish : function (meshes) {
+					onFinish: function (meshes) {
 						if (meshes.length > 0) {
-							viewer.AdjustClippingPlanes (50.0);
-							viewer.FitInWindow ();
+							viewer.AdjustClippingPlanes(50.0);
+							viewer.FitInWindow();
 						}
-						viewer.EnableDraw (true);
-						viewer.Draw ();
+						viewer.EnableDraw(true);
+						viewer.Draw();
 					}
 				};
-				
+
 				var textureLoaded = function () {
-					viewer.Draw ();
+					viewer.Draw();
 				};
-				JSM.ConvertJSONDataToThreeMeshes (jsonData, textureLoaded, environment);	
-			}				
+				JSM.ConvertJSONDataToThreeMeshes(jsonData, textureLoaded, environment);
+			}
 		});
 	}
 
-	var supported = JSM.IsWebGLEnabled () && JSM.IsFileApiEnabled ();
-	var viewers = document.getElementsByClassName ('3dviewer');
-	var i, viewer;
-	for (i = 0; i < viewers.length; i++) {
-		viewer = viewers[i];
-		if (supported) {
-			LoadViewer (viewer);
-		} else {
-			Error (viewer, 'No browser support.');
-		}
+	var supported = JSM.IsWebGLEnabled() && JSM.IsFileApiEnabled();
+	var canvas = document.getElementById('3dcanvas');
+	if (supported) {
+		LoadViewer(canvas);
+	} else {
+		Error(canvas, 'No browser support.');
 	}
 };
