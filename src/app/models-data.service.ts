6326d2a45778068fe24b41f1b6245d2aec10bd93
Mizaroth <domenicosibilio@gmail.com>
2020-10-21 17:52:26 +0200

    Prioritize autocomplete options that start with filter

--- src/app/models-data.service.ts
+++ src/app/models-data.service.ts
@@ -6,49 +6,65 @@ import models from '../assets/weapons.json';
 })
 export class ModelsDataService {
 
-  modelsByAlias = new Map<string, any>()
-  aliasesByCategory = new Map<string, string[]>()
-  aliases = []
+  modelsByAlias = new Map<string, any>();
+  aliasesByCategory = new Map<string, string[]>();
+  aliases = [];
 
-  constructor() { 
+  constructor() {
     models.forEach(model =>
       model.aliases.forEach(alias => {
-        this.modelsByAlias.set(alias, model)
-        this.aliases.push(alias)
+        this.modelsByAlias.set(alias, model);
+        this.aliases.push(alias);
 
-        let currentAliases = this.aliasesByCategory.get(model.category)
-        if(currentAliases === undefined) {
-          currentAliases = [ alias ]
-          this.aliasesByCategory.set(model.category, currentAliases)
+        let currentAliases = this.aliasesByCategory.get(model.category);
+        if (currentAliases === undefined) {
+          currentAliases = [alias];
+          this.aliasesByCategory.set(model.category, currentAliases);
         } else {
-          currentAliases.push(alias)
+          currentAliases.push(alias);
         }
       })
     )
   }
 
   getModels() {
-    return models    
+    return models;
   }
 
   getMatchingAliases(aliasFilter: string): string[] {
-    if(aliasFilter != undefined) {
-      aliasFilter = aliasFilter.toLowerCase()
+    if (aliasFilter != undefined) {
+      aliasFilter = aliasFilter.toLowerCase();
 
-      if(aliasFilter.length > 2)
-        return this.aliases.filter(alias => alias.toLowerCase().indexOf(aliasFilter) != -1)
+      if (aliasFilter.length > 2) {
+        let startsWith = [];
+        let contains = [];
+
+        this.aliases
+          .forEach((alias: string) => {
+            let lowerCaseAlias = alias.toLowerCase();
+            if (lowerCaseAlias.startsWith(aliasFilter)) {
+              startsWith.push(alias);
+            } else if (lowerCaseAlias.indexOf(aliasFilter) != -1) {
+              contains.push(alias);
+            }
+          });
+
+        startsWith.sort();
+        contains.sort();
+        return startsWith.concat(contains);
+      }
     }
 
-    return []
+    return [];
   }
 
   getModelByAlias(alias: string) {
-    return this.modelsByAlias.get(alias)
+    return this.modelsByAlias.get(alias);
   }
 
   getAliasesByCategory(category: string): string[] {
     let result = this.aliasesByCategory.get(category);
-    return result != undefined ? result.slice() : []
+    return result != undefined ? result.slice() : [];
   }
 
 }
